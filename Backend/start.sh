23854da07d02e9134ba2fe01aa3dcce28d678276
Shaun <tyronemas@gmail.com>
2026-06-09 21:18:41 +0200

    Update start.sh

--- Backend/start.sh
+++ Backend/start.sh
@@ -24,5 +24,15 @@ sleep 2
 echo "=== LISTENING SOCKETS ==="
 ss -tulpn
 
+echo "=== NGINX BINARY ==="
+which nginx
+nginx -V 2>&1
+
+echo "=== FIX LOG PERMISSIONS ==="
+mkdir -p /var/log/nginx
+touch /var/log/nginx/error.log /var/log/nginx/access.log
+chmod 777 /var/log/nginx/error.log /var/log/nginx/access.log
+
 echo "=== START NGINX ==="
 nginx -e /dev/stderr -g "daemon off;" 2>&1
+echo "=== NGINX EXIT CODE: $? ==="
