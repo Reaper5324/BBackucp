a1dae0af5e5aa5f20d767703f329c3f71953df42
Shaun <tyronemas@gmail.com>
2026-06-09 16:39:00 +0200

    Update index.php

--- Backend/public/index.php
+++ Backend/public/index.php
@@ -1,17 +1,15 @@
 <?php
 
-$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+header('Content-Type: application/json');
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+echo json_encode([
+    'status' => 'alive',
+    'php' => PHP_VERSION
+]);
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(204);
-    exit;
-}
-
+exit;
 require_once __DIR__ . '/../bater/bootstrap.php';
+require_once __DIR__ . '/../bater/core/Router.php;
 
 $router = new Router();
 
