6e1571b704cf627ca8d9cf2b613296b0c0fd5e89
Shaun <tyronemas@gmail.com>
2026-06-10 08:59:28 +0200

    Update start.sh

--- Backend/start.sh
+++ Backend/start.sh
@@ -4,6 +4,11 @@
 sed -i "s/Listen 80/Listen ${PORT:-80}/" /etc/apache2/ports.conf
 sed -i "s/:80>/:${PORT:-80}>/" /etc/apache2/sites-available/000-default.conf
 
+echo "=== PORTS.CONF ==="
+cat /etc/apache2/ports.conf
+
+echo "=== VHOST CONFIG ==="
+cat /etc/apache2/sites-available/000-default.conf
 
 a2dismod mpm_event 2>/dev/null || true
 a2dismod mpm_worker 2>/dev/null || true
