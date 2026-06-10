e9b94b581cc3d1bb9e60ae530353a2ba698fea9d
Shaun <maswikanengdean@gmail.com>
2026-06-10 08:49:27 +0200

    ghebang fix start.sh

--- Backend/start.sh
+++ Backend/start.sh
@@ -1,4 +1,5 @@
-#!/sh
+#!/bin/sh
+
 # Dynamic port binding for Railway
 sed -i "s/Listen 80/Listen ${PORT:-80}/" /etc/apache2/ports.conf
 sed -i "s/:80>/:${PORT:-80}>/" /etc/apache2/sites-available/000-default.conf
