3e03512dc23bdeb17eeffaa00ea3adf9b3c0daf6
Shaun <tyronemas@gmail.com>
2026-06-17 09:38:57 +0200

    Debug session

--- Backend/public/index.php
+++ Backend/public/index.php
@@ -103,6 +103,27 @@ $router->get('/messages/threads', 'MessageController@threads');
 $router->get('/messages', 'MessageController@thread');
 $router->post('/messages', 'MessageController@send');
 
+$router->get('/debug-session', function () {
+
+    if (session_status() === PHP_SESSION_NONE) {
+
+        session_start();
+
+    }
+
+    echo json_encode([
+
+        'session_id' => session_id(),
+
+        'cookie' => $_COOKIE,
+
+        'session' => $_SESSION,
+
+    ]);
+
+})
+
+
 $router->post('/support', 'SupportController@submit');
 $router->get('/support/my-tickets', 'SupportController@myTickets');
 $router->get('/admin/support', 'SupportController@all');
